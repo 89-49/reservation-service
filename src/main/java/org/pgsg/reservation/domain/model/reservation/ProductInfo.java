489e46c489e7be3f415eda9b36b3519562c0f3fd
김민성 <zsx1397@gmail.com>
2026-05-04 01:51:16 +0900

    Fix : 코드래빗 제안 수정 - endTime은 null을 불가처리

--- src/main/java/org/pgsg/reservation/domain/model/reservation/ProductInfo.java
+++ src/main/java/org/pgsg/reservation/domain/model/reservation/ProductInfo.java
@@ -16,5 +16,6 @@ public class ProductInfo {
     private UUID productId;
     private Integer productPrice;
     private String productName;
+    @Column(nullable = false)
     private LocalDateTime endTime;
 }
